1d9fa3dd608425eb10329e581e4a941e8100a673
sawada <sawada@csplan.jp>
2022-05-17 14:30:16 +0900

    testを作成

---
+++ tests/Unit/閉区間Test.php
@@ -0,0 +1,16 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class 閉区間Test extends TestCase {
+
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public function test_上端点を持つ()
+    {
+        $this->fail('これは、失敗するテストです。');
+    }
+}
