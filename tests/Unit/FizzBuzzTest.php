b955fe31d410bcc1145f34abcf3126c3a8956c6a
sawada <sawada@csplan.jp>
2022-05-17 11:43:19 +0900

    部分的にリファクタ

--- tests/Unit/FizzBuzzTest.php
+++ tests/Unit/FizzBuzzTest.php
@@ -5,18 +5,21 @@ use PHPUnit\Framework\TestCase;
 use  Src\FizzBuzz;
 
 class FizzBuzzTest extends TestCase {
+    private $fizzbuzz;
+
     // 準備
     public function setUp(): void
     {
         parent::setUp();
+
+        $this->fizzbuzz = new FizzBuzz();
     }
 
     public function test_1を”1”へ変換する()
     {
         // 準備
-        $fizzbuzz = new FizzBuzz();
         // 実行 & 検証
-        $this->assertEquals("1", $fizzbuzz->convert(1));
+        $this->assertEquals("1", $this->fizzbuzz->convert(1));
     }
 
     public function test_2を”2”へ変換する()
