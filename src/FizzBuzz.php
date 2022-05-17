ee8412a91bfe5b28c9ed9bcc171da9bacdfacad5
sawada <sawada@csplan.jp>
2022-05-17 11:47:58 +0900

    明白な実装

--- src/FizzBuzz.php
+++ src/FizzBuzz.php
@@ -7,6 +7,8 @@ class FizzBuzz {
     {
         if ($i % 3 == 0) {
             return 'Fizz';
+        } else if ($i % 5 == 0) {
+            return 'Buzz';
         }
         return (string)$i;
     }
